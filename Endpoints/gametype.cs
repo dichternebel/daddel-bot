630913a85e9a3b0249ff87bbb91a732f551b63f4
dichternebel <sasch@dichternebel.de>
2021-01-29 13:27:31 +0100

    Improve map guessing. Try to configure FFA.

--- Endpoints/gametype.cs
+++ Endpoints/gametype.cs
@@ -65,9 +65,32 @@ namespace Rcon.Function
                 var mapList = await RconHelper.GetMaps(rconClient);
 
                 // Set random map that suits the given type or use parameter value(s)
-                var currentMap = rconPayload.Parameter.Length > 1 ?
-                    mapList.FirstOrDefault(x => x.Contains(rconPayload.Parameter[1].ToLower())) :
-                    getRandomMapByGameType(mapList, gameType);
+                var currentMap = String.Empty;
+                if (rconPayload.Parameter.Length < 2)
+                {
+                    currentMap = getRandomMapByGameType(mapList, gameType);
+                }
+                else if (rconPayload.Parameter.Length > 1)
+                {
+                    // exact match
+                    currentMap = mapList.FirstOrDefault(x => x == rconPayload.Parameter[1].ToLower());
+                    // starts with
+                    if (string.IsNullOrEmpty(currentMap))
+                    {
+                        currentMap = mapList.FirstOrDefault(x => x.StartsWith(rconPayload.Parameter[1].ToLower()));
+                    }
+                    // ends with
+                    if (string.IsNullOrEmpty(currentMap))
+                    {
+                        currentMap = mapList.FirstOrDefault(x => x.EndsWith(rconPayload.Parameter[1].ToLower()));
+                    }
+                    // contains -> Warning: could also load de_dust2_se if de_dust2 is parameter
+                    if (string.IsNullOrEmpty(currentMap))
+                    {
+                        currentMap = mapList.FirstOrDefault(x => x.Contains(rconPayload.Parameter[1].ToLower()));
+                    }
+                    // Todo: Fuzzy search? ... No, just kidding. ;-)
+                }
 
                 if (string.IsNullOrEmpty(currentMap))
                 {
@@ -83,7 +106,7 @@ namespace Rcon.Function
                 if (gameType == GameTypes.casual)
                 {
                     var resp = await rconClient.ExecuteCommandAsync($"game_type 0; game_mode 0; exec gamemode_casual; map {currentMap}; mapgroup {currentMapGroup};");
-                    return new OkObjectResult($"Switched server to mode 'Competitive'.\n{resp}");
+                    return new OkObjectResult($"Switched server to mode 'Casual'.\n{resp}");
                 }
                 if (gameType == GameTypes.competitive)
                 {
@@ -107,12 +130,49 @@ namespace Rcon.Function
                 }
                 if (gameType == GameTypes.deathmatch)
                 {
-                    var resp = await rconClient.ExecuteCommandAsync($"game_type 1; game_mode 2; exec gamemode_deathmatch; map {currentMap};mapgroup {currentMapGroup}; sv_infinite_ammo 2; mp_teammates_are_enemies 1;");
-                    return new OkObjectResult($"Switched server to mode 'Deathmatch'.\n{resp}");
+                    // picked from https://github.com/timche/csgo-server-configs/blob/master/cfg/deathmatch.cfg
+                    var resp = await rconClient.ExecuteCommandAsync($@"
+                    game_type 1; game_mode 2; exec gamemode_deathmatch; map {currentMap};mapgroup {currentMapGroup};
+                    sv_infinite_ammo 2;
+                    mp_teammates_are_enemies 1;
+                    mp_timelimit 0;
+                    mp_tkpunish 0;
+                    mp_warmup_pausetimer 1;
+                    mp_warmuptime 1;
+                    bot_quota 0;
+                    mp_friendlyfire 1;
+                    mp_autokick 0;
+                    mp_autoteambalance 0;
+                    mp_buy_allow_grenades 0;
+                    mp_buy_allow_guns 0;
+                    mp_ct_default_grenades "";
+                    mp_ct_default_melee weapon_knife;
+                    mp_ct_default_primary weapon_ak47;
+                    mp_ct_default_secondary weapon_deagle;
+                    mp_t_default_grenades "";
+                    mp_t_default_melee weapon_knife;
+                    mp_t_default_primary weapon_ak47;
+                    mp_t_default_secondary weapon_deagle;
+                    mp_free_armor 2;
+                    mp_death_drop_gun 0;
+                    mp_defuser_allocation 0;
+                    mp_give_player_c4 0;
+                    mp_limitteams 0;
+                    mp_logdetail 0;
+                    mp_match_can_clinch 1;
+                    mp_match_end_restart 1;
+                    mp_randomspawn 1;
+                    mp_respawn_on_death_ct 1;
+                    mp_respawn_on_death_t 1;
+                    ");
+                    return new OkObjectResult($"Switched server to mode 'Deathmatch (FFA)'.\n{resp}");
                 }
                 if (gameType == GameTypes.teamdeathmatch)
                 {
-                    var resp = await rconClient.ExecuteCommandAsync($"game_type 1; game_mode 2; exec gamemode_deathmatch; map {currentMap}; mapgroup {currentMapGroup}; sv_infinite_ammo 2;");
+                    var resp = await rconClient.ExecuteCommandAsync($@"
+                    game_type 1; game_mode 2; exec gamemode_deathmatch; map {currentMap}; mapgroup {currentMapGroup};
+                    sv_infinite_ammo 2;
+                    ");
                     return new OkObjectResult($"Switched server to mode 'Team Deathmatch'.\n{resp}");
                 }
                 
