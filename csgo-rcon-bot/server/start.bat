89b6a8bd57c99a348fd0c666d171b285d9696d8f
dichternebel <sasch@dichternebel.de>
2021-02-04 14:31:06 +0100

    Update batch file to present new env vars.

--- csgo-rcon-bot/server/start.bat
+++ csgo-rcon-bot/server/start.bat
@@ -1,5 +1,6 @@
 SET NODE_ENV=production
 SET BOT_TOKEN=YourVerySecretBotTokenGoesHere
 SET IMG_BASEURL=https://proud-dune-027bff803.azurestaticapps.net/
-SET RCON_CLI_PATH=C:\\rcon-cli\\csgo-rcon-bot\\bin\\rcon-cli.exe
+SET API_KEY=YourApiToken
+SET API_URL=YourApiUrl
 node ..\index.js
