ab5f32ede03db904e0b7107d5d05810e6e795ec3
dichternebel <sasch@dichternebel.de>
2021-12-12 16:13:46 +0100

    Schedule activity. Upgrade some packages.

--- csgo-rcon-bot/app/events/ready.js
+++ csgo-rcon-bot/app/events/ready.js
@@ -1,3 +1,5 @@
+const schedule = require('node-schedule');
+
 module.exports = function (client,config) {
     client.user.setActivity(config.get('PREFIX'), { type: 'LISTENING' });
     // set bot's id
@@ -7,5 +9,11 @@ module.exports = function (client,config) {
     console.log("-----------------------");
     client.guilds.cache.forEach(g => {
         console.log(g.id + " - " + g.name +  "\n|--> joined: " + g.joinedAt + " [" + g.joinedTimestamp + "]");
+    });
+
+    // discord seems to lose activity after some time :-/
+    // this will refresh activity every 3 hours
+    schedule.scheduleJob('*/180 * * * *', function(){
+        client.user.setActivity(config.get('PREFIX'), { type: 'LISTENING' });
     });
 }
